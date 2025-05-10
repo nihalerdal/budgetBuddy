a2e52d1b44227976133b86485f3b1a2810abf447
nihalerdal <nihalerdall@gmail.com>
2025-05-09 17:38:07 -0700

    Implement getAllExpenses

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -3,15 +3,23 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllExpenses = async (req, res) => {
-  res.send("get all expenses");
+  const expenses = await Expense.find({ createdBy: req.user.userId }).sort(
+    "createdAt"
+  );
+  res.status(StatusCodes.OK).json({ expenses, count: expenses.length });
 };
+
 const getExpense = async (req, res) => {
   res.send("get an expense");
 };
-const createExpense = async (req, res) => {
-  req.body.createdBy = req.user.userId;
-  const expense = await Expense.create(req.body);
-  res.status(StatusCodes.CREATED).json({ expense });
+const createExpense = async (req, res, next) => {
+  try {
+    req.body.createdBy = req.user.userId;
+    const expense = await Expense.create(req.body);
+    res.status(StatusCodes.CREATED).json({ expense });
+  } catch {
+    next(error);
+  }
 };
 const updateExpense = async (req, res) => {
   res.send("update expense");
