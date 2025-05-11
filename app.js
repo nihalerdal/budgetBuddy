7caac2b074efbd9018c6830520250e81b84df9e5
nihalerdal <nihalerdall@gmail.com>
2025-05-11 12:40:09 -0700

    fix middleware calls

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(helmet);
-app.use(cors);
-app.use(xss);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
