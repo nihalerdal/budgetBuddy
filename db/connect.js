1bb4f7664d8e4ae5dfd3167642c8933f2c5a3933
nihalerdal <nihalerdall@gmail.com>
2025-05-11 14:18:30 -0700

    minor fix

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDB = (url) => {
-  mongoose
-    .connect(url) 
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => console.log(err));
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = connectDB;
